cbc48945e86f0233a9b35746e858a58a2b3ce55f
saibutubunohakaishinn@gmail.com <dayama@sfc.wide.ad.jp>
2019-12-05 12:56:47 +0900

    1st

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy as sc
+from scipy import linalg
+from scipy import spatial
+import scipy.spatial.distance
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+import pylab
+
+
